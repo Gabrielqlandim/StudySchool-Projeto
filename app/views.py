7e656ae1a5bf962b0198ef7baa268cfe4c9b4097
Lucas Ferreira Torres de Albuquerque <lfta@cesar.school>
2024-02-25 16:24:31 -0300

    adicionado meio correto para colocar fotos e css no site

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def funcao(request):
+def home(request):
     return render(request, 'pages/home.html')
--- projeto/urls.py
+++ projeto/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from app.views import funcao
+from app.views import home
 
 urlpatterns = [
-    path('', funcao),
+    path('', home),
 ]
