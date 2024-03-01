b47edc666700f8394f89f20061a5e128821b2ac6
Gabriel Q S Landim <gqsl@cesar.school>
2024-02-29 21:04:06 -0300

    css e js
    terminei o css e comecei o js

---
+++ app/javascript/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const monthsBR = ['janeiro','fevereiro','março','abril','maio','junho','julho','agosto','setembro','outubro','novembro','dezembro'];
+    console.log(monthsBR);
+    function GetDaysCalendar(mes,ano){
+        
+    }
+    GetDaysCalendar(2,2001);
+})
