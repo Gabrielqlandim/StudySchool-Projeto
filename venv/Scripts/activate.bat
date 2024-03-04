f4b62b04ccba10308d239b4c2d320e4adb5dd7cc
Lukas <lfta@cesar.school>
2024-03-04 10:21:50 -0300

    calendario funcional

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\user\Downloads\Projeto - Lyceum\venv
+set VIRTUAL_ENV=C:\Users\lfta_cesar\Desktop\Lyceum-Projeto\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
