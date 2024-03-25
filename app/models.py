67b66b002186e37286fd28135ac8f7b5265519df
Lukas <lfta@cesar.school>
2024-03-25 12:06:03 -0300

    mudança no banco de dados

--- app/models.py
+++ app/models.py
@@ -3,6 +3,5 @@ from django.db import models
 class Aluno(models.Model):
     id_aluno = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=120)
-    idade = models.CharField(max_length=120)
-    ano = models.CharField(max_length=8)
-    turma = models.CharField(max_length=8)
+    serie_turma = models.CharField(max_length=8)
+    data_matricula = models.DateField()
